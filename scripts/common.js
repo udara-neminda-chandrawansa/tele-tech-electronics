dd1ed8c038abd0414bf8033dafe5defe0b150a60
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-12-04 15:30:57 +0530

    search table method migrated to common scripts

--- scripts/common.js
+++ scripts/common.js
@@ -109,3 +109,27 @@ document.querySelectorAll('.custom-select').forEach(select => {
     const originalSelect = select.querySelector('select');
     new CustomSelect(originalSelect);
 });
+
+
+// for searching tables
+function searchItems(searchInputID, tableID, colNo) {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById(searchInputID);
+    filter = input.value.toUpperCase();
+    table = document.getElementById(tableID);
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[colNo];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
